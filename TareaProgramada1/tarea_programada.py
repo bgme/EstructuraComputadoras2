93e6538cabca62d71588021a76b4559d2ac743db
root <root@cortana94.tigo.co.cr>
2016-04-19 20:21:01 -0600

    se le agregaron las variables de la consola a los tamanios de las partes de la direccion, asi como un par de ajustes pequenos mas

--- TareaProgramada1/tarea_programada.py
+++ TareaProgramada1/tarea_programada.py
@@ -9,23 +9,33 @@ from math import log
 
 miss = 0
 hit = 0
-hola = 1
-prueba = 0
+caso = 0
+
 
 asociatividad = sys.argv[1]
 cache_size = int(sys.argv[2]) #Tamano del cache en KB
 block_size = int(sys.argv[3]) #Tamano del bloque en B
 
+
 num_posiciones = cache_size * 1024 / block_size #numero de bloques de cache de N palabras de 1 byte c/u
 
-byteoffset = int(math.log(block_size,2))
+#aqui se elige la cantidad de bits para cada parte de la direccion (index,tag,byte_offset)
+num_index = int(math.log(num_posiciones,2)) 
+num_byteoffset = int(math.log(block_size,2))
+num_tag = 32 -(num_index+num_byteoffset)
 
+print num_index, num_byteoffset, num_tag
+
+#se le agregan los casos para mayor facilidad de seleccion en el algoritmo posterior
 if asociatividad == 'directo' or asociatividad == 'Directo':
 	memoria = np.empty((num_posiciones, 2)) #Espacio para tag y el indice
+	caso = 1
 elif asociatividad == '2-way' or asociatividad == '2-Way':
 	memoria = np.empty((num_posiciones, 3)) #Espacio para index y los dos tags
+	caso = 2
 elif asociatividad == '4-way' or asociatividad == '4-Way':
 	memoria = np.empty((num_posiciones, 5)) #Espacio para index y los 4 tags
+	caso = 3
 
 ##Orden en memoria
 ##memoria [index, tag1, tag2, tag3, tag4]
@@ -38,27 +48,34 @@ f=open('pruebamemoria','r')
 for line in f:
 	wr = line[-2:]
         dir_bin = bin(int(line[0:8],16))
-	index = dir_bin[-3:]  #esto se tiene que cambiar por variables que dependen de los tamanos introducidos en la consola
-	tag = dir_bin[2:-3]  #igual aqui
+	
+	if num_byteoffset != 0:
+		index = dir_bin[-(num_byteoffset+num_index):-num_byteoffset]
+	else:
+		index = dir_bin[-(num_index):]
+
+	tag = dir_bin[2:-(num_byteoffset+num_index)]  
 	#print dir_bin, index, tag
-        #print int(index,2)
+       
         
         
 	#print wr
         #seccion que revisa cantidad de hits y misses y acomoda el dato en el bloque correspondiente
   
-	#print float(tag),memoria[(int(index,2),1)]
 	
-	if float(tag) == memoria[(int(index,2),1)]:
-		hit = hit+1
-	else:
-		miss = miss+1
-		memoria[(int(index,2),1)] = float(tag)
+	if caso == 1:
+		if float(tag) == memoria[(int(index,2),1)]:
+			hit = hit+1
+		else:
+			miss = miss+1
+			memoria[(int(index,2),1)] = float(tag)
+	#elif caso == 2:
+	#elif caso == 3:
 		
 f.close()
 
 
-np.set_printoptions(precision=8)
+np.set_printoptions(precision=8,edgeitems=5)
 print memoria
 
 
